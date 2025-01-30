e3071487f79f201c6f7bbcdc93031fb49c374bbe
kylo <nareinjunior7@gmail.com>
2025-01-30 10:48:26 +0300

    Re-add all files

---
+++ server.js.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();  
+
+
+const app = express();
+app.use(express.json()); 
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+
+const userRoutes = require('./routes/userRoutes');
+
+
+app.use('/api/users', userRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
