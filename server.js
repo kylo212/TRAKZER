77dffc77ca23df8b5e81f981c2a403e57586ff4c
kylo <nareinjunior7@gmail.com>
2025-01-30 11:54:15 +0300

    Update server.js to use environment variables

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
-const dbURI = process.env.MONGODB_URI;
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("Error connecting to MongoDB:", err));
-
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();  
-
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const dbURI = process.env.MONGODB_URI || process.env.MONGO_URI; 
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-
 const userRoutes = require('./routes/userRoutes');
 
-
 app.use('/api/users', userRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
