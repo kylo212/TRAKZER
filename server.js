161c9bd9ba9240dc08d1658c61b6c648af831bab
kylo <nareinjunior7@gmail.com>
2025-01-30 15:01:07 +0300

    Updated server.js and static file handling

--- server.js
+++ server.js
@@ -7,27 +7,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 const dbURI = process.env.MONGODB_URI;
 
 mongoose.set('strictQuery', false);
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Successfully connected to MongoDB!");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB:", err);
-  });
+  .then(() => console.log("Successfully connected to MongoDB!"))
+  .catch(err => console.log("Error connecting to MongoDB:", err));
 
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Trakzer API!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
