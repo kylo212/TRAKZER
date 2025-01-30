e9a32426bca24275ec2ac36977ca9f746dc53a94
kylo <nareinjunior7@gmail.com>
2025-01-30 13:33:55 +0300

    Updated user route

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
-
 app.use(express.json());
-
 const dbURI = process.env.MONGODB_URI;
-
 console.log("MongoDB URI:", dbURI);
-
 mongoose.set('strictQuery', false);
-
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Successfully connected to MongoDB!");
@@ -19,10 +13,14 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((err) => {
     console.log("Error connecting to MongoDB:", err);
   });
-
+app.get('/', (req, res) => {
+  res.send('Welcome to the Trakzer app! The server is running perfectly.');
+});
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
-
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'Route not found' });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- userRoutes.js
+++ userRoutes.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
-const users = [
-  { id: 1, name: 'Narein Kylo', email: 'nareinjunior7@gmail.com' },
-  { id: 2, name: 'Delick Junior', email: 'nareinkylo@gmail.com' }
-];
-
-
 router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Users list',
-    data: users
-  });
+  res.json({ message: 'Users list fetched successfully', data: [] });
 });
 
 module.exports = router;
