93d90e661066d5908a82b526c9e7a0d3828b39c8
kylo <nareinjunior7@gmail.com>
2025-01-30 13:52:16 +0300

    Updated server.js to handle root route

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+process.on('warning', (warning) => console.warn(warning.name, warning.message));
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 const dbURI = process.env.MONGODB_URI;
-console.log("MongoDB URI:", dbURI);
+
 mongoose.set('strictQuery', false);
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Successfully connected to MongoDB!");
@@ -13,14 +20,14 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((err) => {
     console.log("Error connecting to MongoDB:", err);
   });
-app.get('/', (req, res) => {
-  res.send('Welcome to the Trakzer app! The server is running perfectly.');
-});
+
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
-app.all('*', (req, res) => {
-  res.status(404).send({ message: 'Route not found' });
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Trakzer API!');
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
