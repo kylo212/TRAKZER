b4d09a1cbd5c3492e67f1eef7fda9a2cf1ff049a
kylo <nareinjunior7@gmail.com>
2025-01-30 12:22:54 +0300

    Updated server.js to connect to MongoDB and added .env

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const express = require('express');
+require('dotenv').config();  
+const mongoose = require('mongoose');  
+const express = require('express'); 
+const userRoutes = require('./routes/userRoutes');  
 
 const app = express();
+
+
 app.use(express.json());
 
 
-const dbURI = process.env.MONGODB_URI || process.env.MONGO_URI; 
-
+const dbURI = process.env.MONGODB_URI;  
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes); 
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
