a3609a75a19631f24c4033e35814f09c4f9bd4fb
kylo <nareinjunior7@gmail.com>
2025-01-30 14:34:44 +0300

    Added DM feature with Socket.IO and Leaflet map

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-process.on('warning', (warning) => console.warn(warning.name, warning.message));
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(cors());
 app.use(express.json());
@@ -25,13 +26,24 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send('Welcome to the Trakzer API!');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('sendMessage', (message) => {
+    console.log('Received message:', message);
+    io.emit('receiveMessage', message);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
