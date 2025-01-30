ded8966490562586a1a923eb816df75f3038b760
kylo <nareinjunior7@gmail.com>
2025-01-30 14:40:42 +0300

    Your commit message here

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http = require('http');
-const socketIo = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
 app.use(cors());
 app.use(express.json());
@@ -30,20 +27,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Trakzer API!');
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-
-  socket.on('sendMessage', (message) => {
-    console.log('Received message:', message);
-    io.emit('receiveMessage', message);
-  });
-});
-
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
