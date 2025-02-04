9b530bc6042aa5b99a098a6cf8e664c46ad71084
kylo <nareinjunior7@gmail.com>
2025-02-04 11:29:02 +0300

    Fixed and updated authcontroller.js

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,38 +1,37 @@
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
-const register = async (req, res) => {
+exports.register = async (req, res) => {
+    const { username, password } = req.body;
+
     try {
-        const { username, email, password } = req.body;
-        const userExists = await User.findOne({ email });
-        if (userExists) return res.status(400).json({ message: 'Email already in use' });
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, email, password: hashedPassword });
+        const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
 
-        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.status(201).json({ token });
-    } catch (error) {
-        res.status(500).json({ message: 'Error registering user' });
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-const login = async (req, res) => {
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
     try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ username });
         if (!user) return res.status(400).json({ message: 'User not found' });
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         res.json({ token });
-    } catch (error) {
-        res.status(500).json({ message: 'Error logging in' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
-
-module.exports = { register, login };
