62a7ecdf529ac28fa5ebf169d8639a85132ea65a
kylo <nareinjunior7@gmail.com>
2025-02-04 17:40:56 +0300

    Fixed missing model issue

--- models/message.js
+++ models/message.js
@@ -1,9 +1,29 @@
-const mongoose = require('mongoose');
+const Message = require('../models/Message');
 
-const messageSchema = new mongoose.Schema({
-    senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-}, { timestamps: true });
+const createMessage = async (req, res) => {
+    try {
+        const { senderId, receiverId, content } = req.body;
+        const newMessage = new Message({ senderId, receiverId, content });
+        await newMessage.save();
+        res.status(201).json(newMessage);
+    } catch (error) {
+        res.status(500).json({ message: 'Error sending message' });
+    }
+};
 
-module.exports = mongoose.model('Message', messageSchema);
+const getMessages = async (req, res) => {
+    try {
+        const { senderId, receiverId } = req.query;
+        const messages = await Message.find({
+            $or: [
+                { senderId, receiverId },
+                { senderId: receiverId, receiverId: senderId },
+            ],
+        });
+        res.json(messages);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching messages' });
+    }
+};
+
+module.exports = { createMessage, getMessages };
