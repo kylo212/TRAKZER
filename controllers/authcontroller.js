89a70a03631baf2987795282fda99c456f87b50f
kylo <nareinjunior7@gmail.com>
2025-02-04 16:52:07 +0300

    Updated authController.js

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,48 +1,11 @@
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-exports.register = async (req, res) => {
-    const { username, email, password } = req.body;
-
-    try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword,
-        });
-
-        await newUser.save();
-        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.status(201).json({ token });
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong!' });
+const authController = {
+    register: (req, res) => {
+        res.send("User registered successfully");
+    },
+    login: (req, res) => {
+        res.send("User logged in successfully");
     }
 };
 
-exports.login = async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ message: 'User not found' });
-        }
-
-        const isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if (!isPasswordCorrect) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
-
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.status(200).json({ token });
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong!' });
-    }
-};
+module.exports = authController;
+ 
